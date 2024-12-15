d26acaa2be3db59e3637e1ecde55efa186aa00b4
Md. Shahnewaz Siddique <Md.Shahnewaz.Siddique@optimizely.com>
2024-12-15 15:12:21 +0600

    added refactored solution

--- main.py
+++ main.py
@@ -1,6 +1,124 @@
+from getpass import getpass
+from time import sleep
+from typing import Tuple
 
-def main():
-    pass
+from pyjokes import get_joke
 
-if __name__ == '__main__':
+from util import (
+    NetworkRequest,
+    print_data,
+    print_error,
+    print_info,
+    print_success,
+    print_warning,
+)
+
+API_BASE = "http://localhost:8000/api"
+
+
+def login() -> Tuple[str, str]:
+    print_info("Please login to your account")
+    username = input("Username: ")
+    password = getpass()
+
+    username = "elonmusk"
+    password = "nerd"
+
+    print_warning("Logging in ...")
+
+    try:
+        login_data = {"username": username, "password": password}
+        headers = {"Content-Type": "application/json"}
+        response = NetworkRequest.post(
+            f"{API_BASE}/auth", data=login_data, headers=headers
+        )
+
+        if response["code"] == 200:
+            print_success("Login successful\n")
+            return response["body"]["access_token"], response["body"]["refresh_token"]
+        else:
+            print_error(f"Login failed: {response.get('message', 'Unknown error')}")
+            exit(1)
+    except Exception as e:
+        print_error(f"An error occurred during login: {e}")
+        exit(1)
+
+
+def getNewTweet(existing_tweets) -> str:
+    # NOTE: inifnite loop
+    while True:
+        tweet = get_joke()
+        if tweet not in existing_tweets:
+            return tweet
+
+
+def main() -> None:
+    access_token, refresh_token = login()
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}",
+    }
+
+    print_info("checking recent tweets ...\n")
+
+    tweets_result = NetworkRequest.get(f"{API_BASE}/tweets", headers=headers)
+
+    print_success(f"Time taken: {tweets_result['response_time']} seconds\n")
+
+    print_info("Showing last 5 tweets ...")
+
+    for tweet in tweets_result["body"]:
+        print_data(
+            f"({tweet['id']}) {tweet['author']['firstname'].title()} tweeted at {tweet['created_at']}\n {tweet['text']}",
+        )
+
+    tweets = set([tweet["text"] for tweet in tweets_result["body"]])
+
+    print_info("\nTweets go brrr ...\n")
+
+    for _ in range(10):
+        tweet = getNewTweet(tweets)
+        tweet_data = {"text": tweet}
+
+        post_result = None
+
+        try:
+            post_result = NetworkRequest.post(
+                f"{API_BASE}/tweets", data=tweet_data, headers=headers
+            )
+
+            if post_result["code"] == 401:
+                token_refresh_result = NetworkRequest.post(
+                    f"{API_BASE}/auth/token",
+                    data={"refresh_token": refresh_token},
+                    headers=headers,
+                )
+
+                if token_refresh_result["code"] == 200:
+                    print_warning("Token refreshed")
+                    access_token = token_refresh_result["body"]["access_token"]
+                    refresh_token = token_refresh_result["body"]["refresh_token"]
+
+                    headers["Authorization"] = f"Bearer {access_token}"
+
+                    post_result = NetworkRequest.post(
+                        f"{API_BASE}/tweets", data=tweet_data, headers=headers
+                    )
+
+            if post_result["code"] == 201:
+                print_info(f"Tweeted: {tweet}")
+                print_success(f"Time taken: {post_result['response_time']} seconds\n")
+            else:
+                print_error(
+                    f"Failed to post tweet: {post_result.get('body', 'Unknown error')}"
+                )
+
+        except Exception as e:
+            print_error(f"Error: {e}")
+
+        sleep(1)
+
+
+if __name__ == "__main__":
     main()
--- util.py
+++ util.py
@@ -1,38 +1,110 @@
 import json
+from time import time
 from urllib.request import Request, urlopen
 from urllib.error import HTTPError
 
-"""
-TODO: Write whatever class or function you may need
-but, don't use any third party library
-feel free to make any changes in the given class and its methods' arguments or implementation as you see fit
-"""
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start = time()
+        result = func(*args, **kwargs)
+        end = time()
+        result["response_time"] = end - start
+        return result
+
+    return wrapper
 
 
 class NetworkRequest:
     @staticmethod
-    def get(url, headers = {}):
-        req = Request(url=url, method='GET')
+    @measure_time
+    def request(method, url, data=None, headers={}):
+        req = Request(url=url, method=method)
         for key, value in headers.items():
             req.add_header(key, value)
-        
+
+        if data is not None:
+            req.data = json.dumps(data).encode("utf-8")
+
         result = {}
-        with urlopen(req) as res:
-            body = res.read().decode('utf-8')
-            result['body'] = json.loads(body)
-            result['code'] = res.status
-        
+        try:
+            with urlopen(req) as res:
+                body = res.read().decode("utf-8")
+                result["body"] = json.loads(body)
+                result["code"] = res.status
+        except HTTPError as e:
+            result["body"] = json.loads(e.read().decode("utf-8"))
+            result["code"] = e.code
+
         return result
-    
-    @staticmethod
-    def post():
-        pass
 
     @staticmethod
-    def put():
-        pass
+    @measure_time
+    def get(url, headers={}):
+        return NetworkRequest.request("GET", url, headers=headers)
 
     @staticmethod
-    def delete():
-        pass
+    @measure_time
+    def post(url, data, headers={}):
+        return NetworkRequest.request("POST", url, data=data, headers=headers)
 
+    @staticmethod
+    @measure_time
+    def put(url, data, headers={}):
+        return NetworkRequest.request("PUT", url, data=data, headers=headers)
+
+    @staticmethod
+    @measure_time
+    def delete(url, headers={}):
+        return NetworkRequest.request("DELETE", url, headers=headers)
+
+
+RESET = "\033[0m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+CYAN = "\033[36m"
+MAGENTA = "\033[35m"
+
+
+def colored_output(color):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            print(color, end="")
+            func(*args, **kwargs)
+            print(RESET, end="")
+
+        return wrapper
+
+    return decorator
+
+
+@colored_output(CYAN)
+def print_info(message):
+    print(message)
+
+
+@colored_output(YELLOW)
+def print_warning(message):
+    print(message)
+
+
+@colored_output(GREEN)
+def print_success(message):
+    print(message)
+
+
+@colored_output(RED)
+def print_error(message):
+    print(message)
+
+
+@colored_output(MAGENTA)
+def print_highlight(message):
+    print(message)
+
+
+@colored_output(BLUE)
+def print_data(message):
+    print(message)
